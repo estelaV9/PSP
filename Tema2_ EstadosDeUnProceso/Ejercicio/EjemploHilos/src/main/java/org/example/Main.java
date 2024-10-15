68d2d156ae4fb4a4df36bf61e15726431159af21
estelaV9 <esteladeveg@gmail.com>
2024-10-15 12:01:41 +0200

    llamar al metodo start()

--- Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/Main.java
+++ Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/Main.java
@@ -4,7 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // CREAMOS UN PRODESO
+        // CREAMOS UN PROcESO
         MiProceso p1 = new MiProceso();
+        // NO HACER p1.run PORQUE NO SE EJECUTARIA EL PROCESO EN REALIDAD NO SE EJECUTARIA CONCURRENTEMENTE
+
+        // HAY QUE LLAMAR AL METODO START() PARA LLAMAR AL PROCESO
+        p1.start(); // ESTO AUTOMATICAMENTE LLAMA AL METODO run()
+
     }
 }
--- Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
+++ Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
@@ -4,5 +4,6 @@ public class MiProceso extends Thread {
     @Override
     public void run() {
         super.run(); // WSE EJECUTA AUTOMATICAMENTE
+        System.out.println("Soy el proceso creado");
     } // CUANDO CREEMOS UN PROCESO VA A IR A EL METODO RAN
 }

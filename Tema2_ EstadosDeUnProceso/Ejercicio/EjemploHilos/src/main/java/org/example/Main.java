f871dc889a064f2f77c39acee38b133b995675f1
estelaV9 <esteladeveg@gmail.com>
2024-10-15 12:11:14 +0200

    poner un bucle y comentario de como se ejecuta en el cpu el hilo

--- Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/Main.java
+++ Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/Main.java
@@ -1,15 +1,22 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // CREAMOS UN PROcESO
-        MiProceso p1 = new MiProceso();
-        // NO HACER p1.run PORQUE NO SE EJECUTARIA EL PROCESO EN REALIDAD NO SE EJECUTARIA CONCURRENTEMENTE
+        System.out.println("Hola");
 
+        // CREAMOS UN PROCESO
+        MiProceso p1 = new MiProceso();
+
+        // NO HACER p1.run PORQUE NO SE EJECUTARIA EL PROCESO EN REALIDAD NO SE EJECUTARIA CONCURRENTEMENTE
         // HAY QUE LLAMAR AL METODO START() PARA LLAMAR AL PROCESO
         p1.start(); // ESTO AUTOMATICAMENTE LLAMA AL METODO run()
 
+        // CUANDO CREAS UN PROCESO Y LO LANZAS NO SE EJECUTAS DIRECTAMENTE, LO MANDAS A LA COLA DE LA CPU
+        // Y EL PROCESO QUE ESTE EN LA CPU, EL SIGUE EJECUTANDO
+        // (por eso al ejecutarlo el mensaje del run() se ejecutara en medio del bucle / al principio...
+        // dependiendo el ordenador
+
+        for( int i = 0; i < 100; i++)
+            System.out.println(Thread.currentThread().getName() + " : " + i);
     }
 }

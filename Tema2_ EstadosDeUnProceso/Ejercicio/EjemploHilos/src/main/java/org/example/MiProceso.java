6e3f1b285141e73739bcabf803a76427cb645779
estelaV9 <esteladeveg@gmail.com>
2024-10-15 11:59:19 +0200

    añadir el metodo run y hacer un proceso

--- Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
+++ Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
@@ -1,4 +1,8 @@
 package org.example;
 
-public class MiProceso {
+public class MiProceso extends Thread {
+    @Override
+    public void run() {
+        super.run(); // WSE EJECUTA AUTOMATICAMENTE
+    } // CUANDO CREEMOS UN PROCESO VA A IR A EL METODO RAN
 }

2c549bfd5660ad69dbe2224f9e1e8df4cb88901e
estelaV9 <esteladeveg@gmail.com>
2024-10-15 12:57:34 +0200

    añadir la funcion synchronized y poner un sleep al hilo

--- Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
+++ Tema2_ EstadosDeUnProceso/Ejercicio/EjemploHilos/src/main/java/org/example/MiProceso.java
@@ -10,12 +10,20 @@ public class MiProceso extends Thread {
         System.out.println(Thread.currentThread().getName() + " Soy el proceso creado");
 
         for (; contadorProceso.getNumero() < 1000; ) {
-            System.out.println("Proceso: " + Thread.currentThread().getName() + " - Contador: " + contadorProceso.getNumero());
-            // EL PROCESO 2 PIERDE LA CPU
-            /** se supone que se pierde --> PROBLEMA DE CONCURRENCIA**/
-            int tmp = contadorProceso.getNumero();
-            tmp++;
-            contadorProceso.setNumero(tmp);
+            synchronized (contadorProceso) {
+                System.out.println("Proceso: " + Thread.currentThread().getName() + " - Contador: " + contadorProceso.getNumero());
+                try {
+                    Thread.sleep(1000); // EL PROCESO SE DUERMA x SEGUNDOS (implica perdida de la CPU)
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                // EL PROCESO 2 PIERDE LA CPU
+                /** se supone que se pierde --> PROBLEMA DE CONCURRENCIA**/
+                int tmp = contadorProceso.getNumero();
+                tmp++;
+                contadorProceso.setNumero(tmp);
+            } // CON PARAMETRO DE OBJETO COMPARTIDO PARA NO REPETIR NINGUN NUMERO
+
         } // BUCLE  PARA LANZAR EL NOMBRE Y EL CONTADOR DE PROCESO
     } // CUANDO CREEMOS UN PROCESO VA A IR A EL METODO RUN
 

74273f9771ff69585be4bc732da5e000b0c76633
estelaV9 <esteladeveg@gmail.com>
2024-10-29 12:18:41 +0100

    crear ejercicio progreso de una tarea

---
+++ Tema3_ProgramacionHilosJava/EjercicioHilos/ProgresoTarea/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+/* Progreso de Tarea: Crea un hilo que simule una tarea que dure 10 segundos,
+mostrando el progreso (por ejemplo, cada segundo) en la consola. */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

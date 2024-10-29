6b38fc39bca3837c882165ee06066b2d687fa6c1
estelaV9 <esteladeveg@gmail.com>
2024-10-29 12:12:19 +0100

    crear clase cronometro con el metodo run y hacer un bucle del 1 al 100. Crear un objeto de la clase y llamar al metodo

---
+++ Tema3_ProgramacionHilosJava/EjercicioHilos/CronometroSincronizadoHasta100/src/main/java/org/example/Cronometro.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Cronometro extends java.lang.Thread {
+
+    @Override
+    public void run() {
+        try {
+            for(int i = 1; i <= 100; i++){
+                Cronometro.sleep(1000);
+                System.out.println(i);
+            }
+        } catch (InterruptedException e) {
+            // SI EL HILO ES INTERRUMPIDO, FINALIZA EL HILO
+            Cronometro.currentThread().interrupt();
+        }
+    }
+}
--- Tema3_ProgramacionHilosJava/EjercicioHilos/CronometroSincronizadoHasta100/src/main/java/org/example/Thread.java
+++
@@ -1,4 +0,0 @@
-package org.example;
-
-public class Thread extends java.lang.Thread {
-}

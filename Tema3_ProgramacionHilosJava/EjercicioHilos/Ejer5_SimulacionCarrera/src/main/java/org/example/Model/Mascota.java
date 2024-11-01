7eb66aee3542e3aee027ad1f0dd414e9fbe4174b
estelaV9 <esteladeveg@gmail.com>
2024-11-01 10:54:06 +0100

    modificar la clase mascota y poner solo el atributo de nombre

---
+++ Tema3_ProgramacionHilosJava/EjercicioHilos/Ejer5_SimulacionCarrera/src/main/java/org/example/Model/Mascota.java
@@ -0,0 +1,17 @@
+package org.example.Model;
+
+public class Mascota {
+    private String nombre;
+
+    public Mascota(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}

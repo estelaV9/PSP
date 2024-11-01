1a481900455cdbbbe08b08a7c54ee864a957219b
estelaV9 <esteladeveg@gmail.com>
2024-11-01 11:56:15 +0100

    EJERCICIO TERMINADO: crear las instancias de los objetos e iniciar los hilos

--- Tema3_ProgramacionHilosJava/EjercicioHilos/Ejer6_RetirarDinero/src/main/java/org/example/Main.java
+++ Tema3_ProgramacionHilosJava/EjercicioHilos/Ejer6_RetirarDinero/src/main/java/org/example/Main.java
@@ -1,6 +1,22 @@
 package org.example;
 
+import org.example.Model.Banco;
+import org.example.Model.Cliente;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bienvenido al banco BankPal!");
+
+        Banco banco = new Banco(1000.0); // SE INICIALIZA EL BANCO CON UN SALDO DE 1000
+
+        // CREAR CLIENTES
+        Cliente cliente1 = new Cliente(banco, "Sal Viejo");
+        Cliente cliente2 = new Cliente(banco, "Eugenie");
+        Cliente cliente3 = new Cliente(banco, "Daniele");
+
+        // SE INICIAN LOS HILOS DE LOS CLIENTES
+        cliente1.start();
+        cliente2.start();
+        cliente3.start();
     }
 }

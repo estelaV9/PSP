72c681a74d7242105a7b5e9d2c124f0df70f7d59
estelaV9 <esteladeveg@gmail.com>
2024-12-02 16:59:38 +0100

    meter en el for el metodo synchronized para cumplir con la parte del enunciado

--- Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer1_SincronizacionHilos/src/main/java/org/example/IncrementarContador.java
+++ Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer1_SincronizacionHilos/src/main/java/org/example/IncrementarContador.java
@@ -11,16 +11,18 @@ public class IncrementarContador implements Runnable {
     public void run() {
         // ITERAR 1000 VECES PARA SIMULAR EL INCREMENTO DEL CONTADOR
         for (int j = 0; j < 1000; j++) {
-            // OBTENER EL TIEMPO DE CPU DEL HILO ACTUAL
-            long cpuTime = ManagementFactory.getThreadMXBean().getThreadCpuTime(Thread.currentThread().getId());
-            double cpuTimeInSeconds = cpuTime / 1_000_000_000.0; // CONVERTIR A SEGUNDOS
+            synchronized (SincronizacionHilos.lock) {
+                // OBTENER EL TIEMPO DE CPU DEL HILO ACTUAL
+                long cpuTime = ManagementFactory.getThreadMXBean().getThreadCpuTime(Thread.currentThread().threadId());
+                double cpuTimeInSeconds = cpuTime / 1_000_000_000.0; // CONVERTIR A SEGUNDOS
 
-            // IMPRIMIR EL TIEMPO DE CPU Y EL ID DEL HILO
-            System.out.println("Hilo ID: " + Thread.currentThread().getId() + " - CPU Time: "
-                    + cpuTimeInSeconds + " segundos");
+                // IMPRIMIR EL TIEMPO DE CPU Y EL ID DEL HILO
+                System.out.println("Hilo ID: " + Thread.currentThread().threadId() + " - CPU Time: "
+                        + cpuTimeInSeconds + " segundos");
 
-            // INCREMENTAR EL CONTADOR DE MANERA SEGURA Y EFICIENTE CON AtomicInteger
-            SincronizacionHilos.contador++;
-        }
+                // INCREMENTAR EL CONTADOR DE MANERA SEGURA Y EFICIENTE CON AtomicInteger
+                SincronizacionHilos.contador++;
+            }
+        } // SINCRONIZACION EXPLICITA PARA EVITAR CONDICIONES DE CARRERA
     }
 }

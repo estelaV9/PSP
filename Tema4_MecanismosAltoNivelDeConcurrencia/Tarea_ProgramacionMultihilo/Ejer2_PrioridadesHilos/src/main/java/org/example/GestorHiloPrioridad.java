86062c2bfe25da544c54e4eb72ab64a4f5fb3526
estelaV9 <esteladeveg@gmail.com>
2024-12-02 19:17:33 +0100

    crear la clase GestorHiloPrioridad con el metodo run que contiene la logica para ejecutar primero el hilo prioritario

---
+++ Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer2_PrioridadesHilos/src/main/java/org/example/GestorHiloPrioridad.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class GestorHiloPrioridad implements Runnable {
+    private static final Object lock = new Object(); // ATRIBUTO PARA EL BLOQUEO DE LA SINCRONIZACION
+    private static boolean hiloPrioritarioTerminado = false; // ATRIBUTO PARA CONTROLAR SI EL HILO PRIORITARIO HA TERMINADO
+    private final boolean esPrioritario; // ATRIBUTO PARA SABER SI EL HILO ES PRIORITARIO O NO
+
+    public GestorHiloPrioridad(boolean esPrioritario) {
+        this.esPrioritario = esPrioritario;
+    } // CONSTRUCTOR QUE RECIBE SI EL HILO ES PRIORITARIO
+
+    @Override
+    public void run() {
+        synchronized (lock) {
+            if (!esPrioritario && !hiloPrioritarioTerminado) {
+                try {
+                    lock.wait(); // HILO NO PRIORITARIO ESPERA HASTA QUE LE NOTIFIQUE
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            } // SI EL HILO NO ES PRIORITARIO Y EL HILO PRIORITARIO AUN NO HA TERMINADO, EL BLOQUE ESPERA
+
+            for (int i = 0; i < 5; i++) {
+                try {
+                    System.out.println("Hilo " + Thread.currentThread().getName() +
+                            " con prioridad " + Thread.currentThread().getPriority() +
+                            " está ejecutándose.");
+                    Thread.sleep(500); // EL HILO DUERME MEDIO SEGUNDO
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break; // SI SE INTERRUMPE EL HILO, SE SALE DEL BUCLE
+                }
+            } // SE RECORRE 5 ITERACIONES MOSTRANDO EL MENSAJE DE PRIORIDAD
+
+            if (esPrioritario) {
+                System.out.println("****************");
+                hiloPrioritarioTerminado = true; // INDICA QUE EL HILO PRIORITARIO HA TERMINADO
+                lock.notify(); // SE NOTIFICAN A LOS HILOS QUE ESTAN ESPERANDO EN EL BLOQUEO
+            } // SI ES PRIORITARIO
+        } // BLOQUE DE SINCRONIZACION PARA CONTROLAR EL BLOQUEO LOCK
+    } // METODO RUN
+}

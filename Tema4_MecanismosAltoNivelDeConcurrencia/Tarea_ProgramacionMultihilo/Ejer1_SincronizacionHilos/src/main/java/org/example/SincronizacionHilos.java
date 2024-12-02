923f90e83b052085bdb31c383b52c8193713bdc2
estelaV9 <esteladeveg@gmail.com>
2024-12-02 17:02:40 +0100

    EJERCICIO TERMINADO: trabajar con executors para optimizar el codigo

--- Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer1_SincronizacionHilos/src/main/java/org/example/SincronizacionHilos.java
+++ Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer1_SincronizacionHilos/src/main/java/org/example/SincronizacionHilos.java
@@ -1,22 +1,40 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class SincronizacionHilos {
     public static int contador = 0;
+    public static final Object lock = new Object();
+
     public static void main(String[] args) throws InterruptedException {
-        Thread[] hilos = new Thread[10];
+        // CREAMOS UN ExecutorService CON UN POOL DE 10 HILOS
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        // LANZAR LOS 10 HILOS
         for (int i = 0; i < 10; i++) {
-            hilos[i] = new Thread(new IncrementarContador());
-            hilos[i].start();
-        }
+            executorService.execute(new IncrementarContador());
+        } // CREAMOS VARIOS OBJETOS AL EXECUTOR
 
-        // ESPERAR A QUE TERMINEN LOS HILOS
-        for (Thread hilo : hilos) {
-            hilo.join();
-        }
+        // CERRAMOS EL EXECUTOR PARA QUE NO ACEPTE MAS TAREAS NUEVAS
+        executorService.shutdown();
 
-        // Imprimir el valor final del contador
-        System.out.println("Valor final del contador: " + contador);
+        /* AUNQUE USAMOS shutdown(), NO DEBEMOS ASUMIR QUE TODAS LAS TAREAS TERMINAN
+         * INMEDIATAMENTE. POR ELLO, USAMOS awaitTermination PARA ESPERAR A QUE FINALICEN
+         * TODAS LAS TAREAS O SE ALCANCE EL TIMEOUT. */
+
+        // ATRIBUTO PARA GUARDAR SI TODAS LAS TAREAS HAN FINALIZADO O SE HA CUMPLIDO EL TIMEOUT
+        boolean terminado = executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+        if (terminado) {
+            System.out.println("Valor final del contador: " + contador);
+        } // SI TODAS LAS TAREAS HAN TERMINADO DENTRO DEL TIEMPO, SE MUESTRA EL VALOR DEL CONTADOR
+
+        if (!terminado) {
+            // EN ESTE CASO, UTILIZAMOS shutdownNow() PARA TERMINAR LAS TAREAS INMEDIATAMENTE
+            // A PESAR DE QUE PUEDEN NO HABER SIDO COMPLETADAS
+            executorService.shutdownNow();
+            System.out.println("Valor final del contador: " + contador);
+        } // SI EL EXECUTOR NO HA TERMINADO DENTRO DEL TIMEOUT, SE FORZA A QUE TERMINE
     }
 }

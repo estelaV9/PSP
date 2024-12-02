14d06bccaaa1dd3e242b67597a6b78bea244be7c
estelaV9 <esteladeveg@gmail.com>
2024-12-02 22:10:55 +0100

    EJERCICIO TERMINADO: crear una forma dos de la clase de gestionsuma donde se declaran todos los parametros a utilizar y el metodo run mas sencillo

---
+++ Tema4_MecanismosAltoNivelDeConcurrencia/Tarea_ProgramacionMultihilo/Ejer3_ProgramacionAppMultihilo/src/main/java/org/example/Forma2/GestionSuma.java
@@ -0,0 +1,29 @@
+package org.example.Forma2;
+
+public class GestionSuma implements Runnable {
+    private final int[] listOfNumbers;
+    private final int inicio; // ATRIBUTO QUE INDICA EL INICIO PARA LA SUMA
+    private final int fin; // ATRIBUTO QUE INDICA EL FINAL PARA LA SUMA
+    private final int[] resultado; // ARRAY PARA GUARDAR RESULTADO DE LA MITAD
+    private final int indice; // ÍNDICE PARA GUARDAR RESULTADO EN EL ARRAY
+
+
+    public GestionSuma(int[] listOfNumbers, int inicio, int fin, int[] resultado, int indice) {
+        this.listOfNumbers = listOfNumbers;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.resultado = resultado;
+        this.indice = indice;
+    } // CONSTRUCTOR CON TODOS LOS PARAMETROS
+
+    @Override
+    public void run() {
+        int sumaParcial = 0; // SE INICIALIZA LA SUMA PARCIAL
+
+        for (int i = inicio; i < fin; i++) {
+            sumaParcial += listOfNumbers[i];
+        } // SE SUMA LOS NUMEROS EN EL RANGO ESPECIFICADO
+
+        resultado[indice] = sumaParcial; // SE GUARDA EN EL ARRAY DE RESULTADOS
+    } // METODO RUN
+}
